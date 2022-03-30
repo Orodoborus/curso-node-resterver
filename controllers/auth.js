f1af5a73d40ae39464e8faf48bb6f1e66d109c1e
Dandre <d.high@hotmail.com>
2022-03-30 16:34:26 -0600

    Google SignIn added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const bcryptjs = require("bcryptjs");
 const { response } = require("express");
 const { generarJWT } = require("../helpers/generarJWT");
+const { googleVerify } = require("../helpers/google-verify");
 const Usuario = require('../models/user');
 
 
@@ -40,6 +41,51 @@ const login = async (req, res = response) => {
     }
 }
 
+const googleSignIn = async (req, res = response) =>{
+    const { id_token } = req.body;
+
+    try {
+        const { correo, nombre, img } = await googleVerify( id_token );
+        
+        let usuario = await Usuario.findOne({correo});
+
+        if( !usuario ){
+            //crear nuevo usuario
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                rol: 'USER:ROLE',
+                google: true
+            };
+
+            usuario = new Usuario( data );
+            await usuario.save();
+        }
+
+        //Si el usuario en DB estado : false
+        if( !usuario.estado )
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            })
+
+        //Generar el JWT
+        const token = await generarJWT( usuario.id );
+
+        res.json({
+            usuario,
+            token
+        })
+
+    } catch (error) {
+        res.status(400).json({
+            msg: 'Verificacion de google falló.'
+        })
+    }
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
